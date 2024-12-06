3464ec64e2afc37561c01a9ead0a39a897b39eb5
Daniel344712 <156951237+Daniel344712@users.noreply.github.com>
2024-12-06 00:59:56 -0600

    PDF arreglado

--- proyecto/Portafolio/index.js
+++ proyecto/Portafolio/index.js
@@ -1,17 +1,5 @@
 
 
-document.querySelectorAll('dotlottie-player').forEach(player => {
-  player.addEventListener('click', () => {
-    const filePath = player.getAttribute('data-file');
-    // Inicia la descarga del archivo PDF
-    const link = document.createElement('a');
-    link.href = filePath;
-    link.download = filePath.split('/').pop();
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  });
-});
 
 
 
